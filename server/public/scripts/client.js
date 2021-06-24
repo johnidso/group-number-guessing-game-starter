f24fb46e31d1c3c509023552a9fe9b6554c76030
John Idso <john.d.idso@gmail.com>
2021-06-24 18:09:51 -0500

    added then and catch to submitGuesses function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,25 @@ $(document).ready(handleReady);
 function handleReady() {
   console.log("jquery is loaded!")
 }
+
+function submitGuesses(){
+  $.ajax({
+    method: 'POST',
+    url: '/submit',
+    data: {
+      guessSet:{
+        playerOne: $('#playerOneIn').val(),
+        playerTwo: $('#playerTwoIn').val(),
+        playerThree: $('#playerThreeIn').val(),
+        playerFour: $('#playerFourIn').val() 
+      }
+    }
+  })
+  .then(function(response){
+    // we may want to add the print to DOM function in here?
+    // as for my tasks, I don't think there's anything for me to do here -JI
+  })
+  .catch(function(error){
+    alert('request failed');
+  });
+}
