b512b26fa9e7fc2a3af4cf99af70d51ae67f0009
Tim Dugan <tadugan@me.com>
2021-06-24 19:37:46 -0500

    installed node modules, fixed body parser in server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
-
+app.use(express.json());
 // GET & POST Routes go here
 
 
