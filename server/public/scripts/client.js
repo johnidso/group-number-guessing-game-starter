b4a2cac7b241bfe2ff726ba239a27979491977f5
Meghan Cleland <mplscleland@gmail.com>
2021-06-25 10:32:29 -0500

    adding my render function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -27,10 +27,40 @@ function submitGuesses(){
     } 
   })
   .then(function(response){
-    // we may want to add the print to DOM function in here?
-    // as for my tasks, I don't think there's anything for me to do here -JI
+   console.log('The guess responses are', response)
+   //append the guesses to the DOM
+   renderGuesses(response);
   })
   .catch(function(error){
     alert('request failed');
   });
 }
+//wondering the best way to break this down for each player
+function renderGuesses(guessSet){
+  //turn into an array
+  console.log(guessSet);
+
+  for (player in playerAnswers){
+    let playOneHistory =[];
+    let playerTwoHistory =[];
+    let playerThreeHistory =[];
+    let playerFourHistory = [];
+    if(player.name = playerOne){
+      playerOneHistory.push(player.number, playerAnswer)
+    }else if(player.name = playerTwo){
+      playerTwoHistory.push(player.number)
+    }else if(player.name = playerThree){
+      playerThreeHistory.push(player.name, playerAnswer)
+    }else if(player.name = playerFour){
+      playerFourHistory.push(player.name, playerAnswer)
+    }else console.log('error')
+
+    
+  }
+  return $('div').append(`
+  <div>playerOneHistory</div>
+  <div>playerTwoHistory</div>
+  <div>playerThreeHistory</div>
+  <div>playerFourHistory</div>`);
+};
+ 
